38497bc942012e96d6a0aacd42b619c5f7e9cdbe
abhaymaurya57 <abhaymaurya54321@gmail.com>
2026-06-29 18:17:48 +0530

    basic channel

--- channel_intig/app/consumers.py
+++ channel_intig/app/consumers.py
@@ -9,8 +9,9 @@ class MyAsyncConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data=None, bytes_data=None):
         print("📨 Received from client:", text_data)
         for i in range(50):
+            print(i)
             await self.send(text_data=str(i))
-            await asyncio.sleep(1.4)
+            await asyncio.sleep(1.5)
 
     async def disconnect(self, close_code):
         print(f"❌ WebSocket disconnected with code {close_code}")
